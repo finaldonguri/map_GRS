586683bb4abd6bd3838ce2696802b172445b34c3
finaldonguri <djungo@gmail.com>
2025-11-01 21:11:42 +0900

    Japan Regional TerrainとGoogle Maps 2D Satellite with Labelsに変更

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 // Grant CesiumJS access to your ion assets
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyOGRiZmY3Yy0wNzRjLTQ2MjktOGQ0Ni0xYmI5MzFmNDUxZDAiLCJpZCI6MzU0MDY0LCJpYXQiOjE3NjE0NTQ3MDh9.p9q4yTuNNbVz7U09nx04n-LQG0sxXh8TDw22H3FSIV0";
+Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyOGRiZmY3Yy0wNzRjLTQ2MjktOGQ0Ni0xYmI5MzFmNDUxZDAiLCJpZCI6MzU0MDY0LCJpYXQiOjE3NjE0NTQ3MDh9.p9q4yTuNNbVz7U09nx04n-LQG0sxXh8TDw22H3FSIV0";
 
 // Cesium Viewerの初期化
 // Cesium 1.99対応版：Cesium World Terrainを使用
-var viewer = new Cesium.Viewer('cesiumContainer', {
+var viewer = new Cesium.Viewer("cesiumContainer", {
     terrainProvider: new Cesium.CesiumTerrainProvider({
-        url: Cesium.IonResource.fromAssetId(1) // Cesium World Terrain
+        url: Cesium.IonResource.fromAssetId(1), // Cesium World Terrain
     }),
     baseLayerPicker: true,
-    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2 }) // Bing Maps Aerial
+    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2 }), // Bing Maps Aerial
 });
 
-
 const layer = viewer.imageryLayers.addImageryProvider(
-    await Cesium.IonImageryProvider.fromAssetId(3),
+    await Cesium.IonImageryProvider.fromAssetId(3830183),
 );
 
 viewer.scene.setTerrain(
-    new Cesium.Terrain(
-        Cesium.CesiumTerrainProvider.fromIonAssetId(1),
-    ),
+    new Cesium.Terrain(Cesium.CesiumTerrainProvider.fromIonAssetId(2767062)),
 );
-
 
 // 初期表示時のカメラ位置（大阪付近）
 viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(135.70, 35.22, 10000.0),
-    duration: 2
+    destination: Cesium.Cartesian3.fromDegrees(135.7, 35.22, 10000.0),
+    duration: 2,
 });
