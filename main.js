08c9f68e7b99dbc9ce4ab82d27407bc5b6b7d31c
finaldonguri <djungo@gmail.com>
2025-11-02 23:28:13 +0900

    style: Cesium.LabelStyle.FILL_AND_OUTLINE,

--- main.js
+++ main.js
@@ -23,14 +23,15 @@ Cesium.Ion.defaultAccessToken =
     viewer.entities.add({
         position: Cesium.Cartesian3.fromDegrees(135.7082915, 35.2232903),
         point: {
-            pixelSize: 12,
+            pixelSize: 8,
             color: Cesium.Color.RED,
             outlineColor: Cesium.Color.WHITE,
             outlineWidth: 2
         },
         label: {
-            text: 'Ginreisou house',  // ← ここを好きなテキストに変更できます
+            text: 'Ginreisou house', 
             font: '12pt sans-serif',
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
             fillColor: Cesium.Color.WHITE,
             outlineColor: Cesium.Color.BLACK,
             outlineWidth: 3
