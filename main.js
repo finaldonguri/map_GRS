dde4b2530773665460785f3e42ccf45c26fd1dd5
finaldonguri <djungo@gmail.com>
2025-11-02 21:59:26 +0900

    infoDivの削除2. 閉じ括弧の修正3. エラーハンドリング追加4. カメラ位置の改善

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Grant CesiumJS access to your ion assets
+// Cesium ionのアクセストークン
 Cesium.Ion.defaultAccessToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyOGRiZmY3Yy0wNzRjLTQ2MjktOGQ0Ni0xYmI5MzFmNDUxZDAiLCJpZCI6MzU0MDY0LCJpYXQiOjE3NjE0NTQ3MDh9.p9q4yTuNNbVz7U09nx04n-LQG0sxXh8TDw22H3FSIV0";
 
@@ -19,31 +19,29 @@ Cesium.Ion.defaultAccessToken =
     const terrainProvider = await Cesium.CesiumTerrainProvider.fromIonAssetId(2767062);
     viewer.terrainProvider = terrainProvider;
 
-
-
-    infoDiv.innerHTML = '<h3>高室山ルート</h3><p>GeoJSON読み込み中...</p>';
-
-    var routePath = 'route.geojson';
+    // GeoJSONルートの読み込み
+    const routePath = 'route.geojson';
     console.log('GeoJSON読み込み開始:', routePath);
 
-    Cesium.GeoJsonDataSource.load(routePath).then(function (dataSource) {
+    try {
+        const dataSource = await Cesium.GeoJsonDataSource.load(routePath);
         console.log('GeoJSON読み込み成功');
-
         viewer.dataSources.add(dataSource);
 
-        var entities = dataSource.entities.values;
+        const entities = dataSource.entities.values;
         console.log('エンティティ数:', entities.length);
 
-        for (var i = 0; i < entities.length; i++) {
-            var entity = entities[i];
+        for (let i = 0; i < entities.length; i++) {
+            const entity = entities[i];
 
+            // ルート線の設定
             if (entity.polyline) {
                 entity.polyline.material = Cesium.Color.RED;
                 entity.polyline.width = 5;
-                // clampToGround は古いビルドだと無いことがあるので、必要なら後で戻す
-                // entity.polyline.clampToGround = true;
+                entity.polyline.clampToGround = true;
             }
 
+            // ポイントの設定
             if (entity.position) {
                 entity.point = new Cesium.PointGraphics({
                     pixelSize: 10,
@@ -52,11 +50,13 @@ Cesium.Ion.defaultAccessToken =
                     outlineWidth: 2
                 });
 
+                // スタート地点（緑）
                 if (i === 0) {
                     entity.point.pixelSize = 15;
                     entity.point.color = Cesium.Color.GREEN;
                 }
 
+                // ゴール地点（青）
                 if (i === entities.length - 1) {
                     entity.point.pixelSize = 15;
                     entity.point.color = Cesium.Color.BLUE;
@@ -64,12 +64,16 @@ Cesium.Ion.defaultAccessToken =
             }
         }
 
+        // ルートが読み込まれたらカメラをルートに合わせる
+        viewer.flyTo(dataSource);
 
+    } catch (error) {
+        console.error('GeoJSON読み込みエラー:', error);
+    }
 
-    // 初期表示時のカメラ位置（大阪付近）
-    viewer.camera.flyTo({
-        destination: Cesium.Cartesian3.fromDegrees(135.7, 35.22, 10000.0),
-        duration: 2,
-    });
-
+    // 初期表示時のカメラ位置（ルートが読み込まれない場合のフォールバック）
+    // viewer.camera.flyTo({
+    //     destination: Cesium.Cartesian3.fromDegrees(135.7, 35.22, 10000.0),
+    //     duration: 2
+    // });
 })();
