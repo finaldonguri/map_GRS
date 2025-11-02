9b6567827e287ddc2a65a0b3dad4c7967ba4089e
finaldonguri <djungo@gmail.com>
2025-11-02 23:15:56 +0900

    route.geojsonでなくGRSの位置ラベル＆ズームのみで

--- main.js
+++ main.js
@@ -19,55 +19,27 @@ Cesium.Ion.defaultAccessToken =
     const terrainProvider = await Cesium.CesiumTerrainProvider.fromIonAssetId(2767062);
     viewer.terrainProvider = terrainProvider;
 
-    // GeoJSONルートの読み込み
-    const routePath = 'route.geojson';
-    console.log('GeoJSON読み込み開始:', routePath);
 
-    try {
-        const dataSource = await Cesium.GeoJsonDataSource.load(routePath);
-        console.log('GeoJSON読み込み成功');
-        viewer.dataSources.add(dataSource);
-
-        const entities = dataSource.entities.values;
-        console.log('エンティティ数:', entities.length);
-
-        for (let i = 0; i < entities.length; i++) {
-            const entity = entities[i];
-
-            // ルート線の設定
-            if (entity.polyline) {
-                entity.polyline.material = Cesium.Color.RED;
-                entity.polyline.width = 5;
-                entity.polyline.clampToGround = true;
-            }
-
-            // ポイントの設定
-            if (entity.position) {
-                entity.point = new Cesium.PointGraphics({
-                    pixelSize: 10,
-                    color: Cesium.Color.YELLOW,
-                    outlineColor: Cesium.Color.BLACK,
-                    outlineWidth: 2
-                });
-
-                // スタート地点（緑）
-                if (i === 0) {
-                    entity.point.pixelSize = 15;
-                    entity.point.color = Cesium.Color.GREEN;
-                }
-
-                // ゴール地点（青）
-                if (i === entities.length - 1) {
-                    entity.point.pixelSize = 15;
-                    entity.point.color = Cesium.Color.BLUE;
-                }
-            }
+    viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(135.7082915, 35.2232903),
+        point: {
+            pixelSize: 15,
+            color: Cesium.Color.RED,
+            outlineColor: Cesium.Color.WHITE,
+            outlineWidth: 2
+        },
+        label: {
+            text: 'Ginreisou house',  // ← ここを好きなテキストに変更できます
+            font: '16pt sans-serif',
+            fillColor: Cesium.Color.WHITE,
+            outlineColor: Cesium.Color.BLACK,
+            outlineWidth: 3
         }
+    });
 
-        // ルートが読み込まれたらカメラをルートに合わせる
-        viewer.flyTo(dataSource);
-
-    } catch (error) {
-        console.error('GeoJSON読み込みエラー:', error);
-    }
+    // 銀嶺荘にカメラを向ける
+    viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(135.7082915, 35.2232903, 2000.0),
+        duration: 2
+    });
 })();
