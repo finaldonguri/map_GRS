f6ddf7c4eca88def26aa71ab4b00000befdfe26c
finaldonguri <djungo@gmail.com>
2025-11-02 21:53:03 +0900

    rout.geojsonを追加してみた

--- main.js
+++ main.js
@@ -20,6 +20,52 @@ Cesium.Ion.defaultAccessToken =
     viewer.terrainProvider = terrainProvider;
 
 
+
+    infoDiv.innerHTML = '<h3>高室山ルート</h3><p>GeoJSON読み込み中...</p>';
+
+    var routePath = 'route.geojson';
+    console.log('GeoJSON読み込み開始:', routePath);
+
+    Cesium.GeoJsonDataSource.load(routePath).then(function (dataSource) {
+        console.log('GeoJSON読み込み成功');
+
+        viewer.dataSources.add(dataSource);
+
+        var entities = dataSource.entities.values;
+        console.log('エンティティ数:', entities.length);
+
+        for (var i = 0; i < entities.length; i++) {
+            var entity = entities[i];
+
+            if (entity.polyline) {
+                entity.polyline.material = Cesium.Color.RED;
+                entity.polyline.width = 5;
+                // clampToGround は古いビルドだと無いことがあるので、必要なら後で戻す
+                // entity.polyline.clampToGround = true;
+            }
+
+            if (entity.position) {
+                entity.point = new Cesium.PointGraphics({
+                    pixelSize: 10,
+                    color: Cesium.Color.YELLOW,
+                    outlineColor: Cesium.Color.BLACK,
+                    outlineWidth: 2
+                });
+
+                if (i === 0) {
+                    entity.point.pixelSize = 15;
+                    entity.point.color = Cesium.Color.GREEN;
+                }
+
+                if (i === entities.length - 1) {
+                    entity.point.pixelSize = 15;
+                    entity.point.color = Cesium.Color.BLUE;
+                }
+            }
+        }
+
+
+
     // 初期表示時のカメラ位置（大阪付近）
     viewer.camera.flyTo({
         destination: Cesium.Cartesian3.fromDegrees(135.7, 35.22, 10000.0),
